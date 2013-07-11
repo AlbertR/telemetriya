07bf6df909b4b09e5c86db801a2d1a0e50b2b2d1
AlbertR <igor.ushakov.cg@gmail.com>
2013-07-11 13:50:50 +0400

    Correct engine

--- engine/engine.py
+++ engine/engine.py
@@ -69,11 +69,15 @@ def initdb(engine):
     
     class Roles_Users(Base):
         __tablename__ = 'roles_users'
-        
         user_id = schema.Column(types.Integer, schema.ForeignKey('user.id'))
         role_id = schema.Column(types.Integer, schema.ForeignKey('role.id'))
-    
+        #__table_args__ = {PrimaryKeyConstraint(user_id,role_id),}
+        __mapper_args__ = {"primary_key":(user_id,role_id)}
+        
     class Role(Base):
+
+        __tablename__ = 'role'
+        
         id = schema.Column(types.Integer(), primary_key=True)
         name = schema.Column(types.String(80), unique=True)
         description = schema.Column(types.String(255))
@@ -89,7 +93,7 @@ def initdb(engine):
         active = schema.Column(types.Boolean())
         confirmed_at = schema.Column(types.DateTime())
         user_info = schema.Column(types.Integer, schema.ForeignKey('user_info.id'))
-        roles = relationship('Role', secondary=roles_users, backref=backref('users', lazy='dynamic'))
+        #roles = relationship('Role', secondary=roles_users, backref=backref('users', lazy='dynamic'))
 
         def __init__(self, login, password, user_tag, user_phone, active, confirmed_at, user_info, roles):
             self.login = login
@@ -111,7 +115,7 @@ def initdb(engine):
     
         id = schema.Column(types.Integer, schema.Sequence('user_id', optional=True), primary_key=True)
         #user = db.Column(db.Integer, db.ForeignKey('user.id'))
-        user = relationship('User', uselist=False, backref='users_info', cascade_backrefs=True)
+        user = relationship('TUser', uselist=False, backref='users_info', cascade_backrefs=True)
         user_lname = schema.Column(types.Unicode(255), nullable=False, unique=False, default=u'')
         user_fname = schema.Column(types.Unicode(255), nullable=False, unique=False, default=u'')
         user_patronymic = schema.Column(types.Unicode(255), nullable=True, unique=False, default=u'')
@@ -141,7 +145,7 @@ def initdb(engine):
 
         id = schema.Column(types.Integer, schema.Sequence('statistics_id', optional=True), primary_key=True)
         date = schema.Column(types.DateTime(), default=now())
-        label = schema.Column(types.Integer, default = 0)
+        label_id = schema.Column(types.Integer, default = 0)
         user_id = schema.Column(types.Integer, default = 0)
         ringtime = schema.Column(types.Float, default = 0)
         track_id = schema.Column(types.Integer)
@@ -149,7 +153,7 @@ def initdb(engine):
 
         def __init__(self, date, label, user_id, ringtime, track_id, session):
             self.date = date
-            self.label = label
+            self.label_id = label
             self.user_id = user_id
             self.ringtime = ringtime
             self.track_id = track_id
@@ -206,8 +210,8 @@ def fillDB(engine, TBase, date, label, ringtime):
 
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    baseData = TBase(date, label, 9, ringtime, 1, 1)
+    print label
+    baseData = TBase(date, label, 2, ringtime, 1, 1)
     session.add(baseData)
     session.commit()
 
@@ -218,11 +222,12 @@ def time2str(time):
 def get_data(engine, TBase, port, baudrate=115200, timeout=1):
     '''
     '''
-    ser = serial.serial_for_url('COM12', baudrate=baudrate, timeout=timeout) # Open com port
+    ser = serial.serial_for_url(port, baudrate=baudrate, timeout=timeout) # Open com port
     t1old = 0                   # Initialization
     t2old = 0                   # Initialization
     while 1:                    # While TRUE read a com port
         data = ser.read(14)     # Read 14 bytes
+        #print data
         if (data != '') and (data != []): # Check for data
             #print 'data: ',repr(data)
             try:
@@ -255,15 +260,16 @@ def main():
 
     # Options to access the database
     #dbparam = 'mysql://root:ar1m2312@192.168.0.88:3306/_telemetriya?charset=utf8&use_unicode=1'
-    #dbparam = 'mysql://root:1234@127.0.0.1:3306/telemetriya?charset=utf8&use_unicode=1'
-    dbparam = 'mysql://root:30v11aiR@178.132.203.168:3306/_telemetriya?charset=utf8&use_unicode=1'
+    dbparam = 'mysql://root:1234@127.0.0.1:3306/_telemetriya?charset=utf8&use_unicode=1'
+    #dbparam = 'mysql://root:30v11aiR@178.132.203.168:3306/_telemetriya?charset=utf8&use_unicode=1'
     engine = create_engine(dbparam, echo=False)
     TUser, TBase, TTrack = initdb(engine)
 
-    print TUser, TBase, TTrack
+    #print TUser, TBase, TTrack
 
     #
-    get_data(engine, TBase, 'COM12', 115200, 1)
+    #get_data(engine, TBase, 'COM12', 115200, 1)
+    get_data(engine, TBase, 'COM3', 115200, 1)
     #print TBase
 
 # ''' when run from the IDE to comment out the following two lines and uncomment the third '''
